5553acbceb302b8428dd47ea9c77720a409d8647
Jonathan Kirkpatrick <jone.kirkpatrick@gmail.com>
2021-04-05 16:26:23 -0700

    Added basic mobile menu

--- Components/Layout/Layout.js
+++ Components/Layout/Layout.js
@@ -1,17 +1,26 @@
 import styles from "./Layout.module.css";
 import Head from "next/head";
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ModeContext } from "../../Context/context";
 
 export default function Layout({ children, title = "Evron.dev :: Blog" }) {
   const mainContext = useContext(ModeContext);
+  const [drawerOpen, setDrawer] = useState(false);
+
+  const drawerClickHandler = () => {
+    setDrawer(!drawerOpen);
+  };
+
   return (
     <div
       className={
         mainContext.lightMode ? styles.container : styles.containerLight
       }
     >
+      {drawerOpen ? (
+        <div className={styles.modal} onClick={() => setDrawer(false)}></div>
+      ) : null}
       <Head>
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
@@ -24,21 +33,22 @@ export default function Layout({ children, title = "Evron.dev :: Blog" }) {
 
       <header className={styles.navbar}>
         <img onClick={mainContext.switch} src="/logo2.png" />
-        <div className={styles.navLinks}>
+        <div className={styles.drawer} onClick={drawerClickHandler}></div>
+        <div className={drawerOpen ? styles.drawerOpen : styles.navLinks}>
           <Link href="/">
-            <a>Home</a>
+            <a className={styles.Links}>Home</a>
           </Link>
           <Link href="/allPosts">
-            <a>All Posts</a>
+            <a className={styles.Links}>All Posts</a>
           </Link>
           <Link href="/chron">
-            <a>Chronological</a>
+            <a className={styles.Links}>Chronological</a>
           </Link>
           <Link href="/topics">
-            <a>Topics</a>
+            <a className={styles.Links}>Topics</a>
           </Link>
           <Link href="/post">
-            <a>Post</a>
+            <a className={styles.Links}>Post</a>
           </Link>
         </div>
       </header>
